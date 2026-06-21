af809b132c3d16f034f3488ec551e1002841343f
timmyrawr <timmypham6@gmail.com>
2026-06-21 00:25:00 -0400

    maincpde

--- app/src/main/java/appjava/MainApp.java
+++ app/src/main/java/appjava/MainApp.java
@@ -1,7 +1,9 @@
 package appjava;
 
 import java.util.*;
-
+import javafx.animation.AnimationTimer;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +13,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
@@ -19,34 +22,49 @@ import javafx.stage.Stage;
 import javafx.scene.canvas.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 public class MainApp extends Application {
-    int height = 1080;
-    int width = 1920;
-
-    final int GRAVITY = -5;
-    int gravity = GRAVITY;
-
-    boolean canJump = false;
+    double height = 915;
+    double width = 412;
     double elapsedSeconds = 0;
-
-    public static enum move {
-        Left,
-
-        Right,
-        Jump,
-        None;
-    }
-
-
-    move Dir = move.None;
+    int scoreCount = 0;
+    String conversion = "0";
 
 
     @Override
     public void start(Stage stage) throws Exception {
-        VBox root = new VBox(); //allows the arrangement of things on the app
+        Pane root = new Pane(); //allows the arrangement of things on the app
         Canvas canva = new Canvas(width, height); //creates grid
         GraphicsContext gc = canva.getGraphicsContext2D(); //allows creation of objects
-        StickFigure stick = new StickFigure();
+        Player tung = new Player(0,200);
+
+        Label score = new Label("Score: 0"); //creates a score label
+
+        score.setLayoutX(275); //moves to top  right
+        score.setLayoutY(20);
+        score.setFont(new Font("Arial", 24));
+        score.setTextFill(Color.RED);
+
+        Circle circle = new Circle(40);
+
+        circle.setFill(Color.LIGHTGRAY);
+
+        Label jumpLabel = new Label("Jump");
+
+        StackPane jump = new StackPane(circle, jumpLabel); //stacks the label above the circle, to create abutton
+
+        jump.setOnMousePressed(e -> {
+            if(tung.canJump){
+                tung.isJump();
+                tung.canJump = false;
+            }
+        });
+        jump.setLayoutX(20);
+        jump.setLayoutY(700);
+
+        root.getChildren().add(canva);
+        root.getChildren().add(score);
+        root.getChildren().add(jump);
 
         AnimationTimer timer = new AnimationTimer() {
 
@@ -55,46 +73,32 @@ public class MainApp extends Application {
 
             @Override
             public void handle(long now) {
-                if (startTime == 0) {
-                    startTime = now;
 
-                    return;
-                }
+                scoreCount++; //each frame, increment score by 1;
+                conversion = Integer.toString(scoreCount);
+                score.setText("Score: " + conversion); //changes the more you play.
+                tung.update(gc);
+
                 // elapsedSeconds = (now - startTime) / 1_000_000_000.0;
 
                 // Update Loop
-                stick.update(gc);
+
 
             }
         };
         timer.start();
-        stick.draw(gc);
-        root.getChildren().add(canva);
+
+
         Scene scene = new Scene(root, width, height);
-        scene.addEventFilter(KeyEvent.KEY_PRESSED, key -> {
-            if (key.getCode() == KeyCode.W) {
-                Dir = move.Jump;
-            } else if (key.getCode() == KeyCode.D) {
-                Dir = move.Right;
-            } else if (key.getCode() == KeyCode.A) {
-                Dir = move.Left;
-            } else if (key.getCode() == KeyCode.S) {
 
-            }
 
-        });
-        scene.addEventFilter(KeyEvent.KEY_RELEASED, key -> {
-            if (key.getCode() == KeyCode.BACK_SPACE || key.getCode() == KeyCode.D || key.getCode() == KeyCode.A || key.getCode() == KeyCode.W) {
-                Dir = move.None;
-            }
-
-        });
         // Showing Everything
         stage.setTitle("JavaFX and Gradle");
         stage.setScene(scene);
         stage.show();
 
     }
+
     public class Player {
         javafx.scene.image.Image playerImage;
         int yVel = 0;
@@ -103,95 +107,46 @@ public class MainApp extends Application {
         int yImage;
         int pastX = 0;
         int pastY = 0;
-        final int width = 500;
-        final int height = 400;
+        final int width1 = 300;
+        final int height1 = 500;
+        final int gravity = 1;
+        boolean canJump = true;
+        public Player (int xImage, int yImage){
+            playerImage = new javafx.scene.image.Image(getClass().getResourceAsStream("/rwew-removebg-preview.png"));
+            this.xImage = xImage;
+            this.yImage = yImage;
+        }
+        public void isJump (){
+            yVel = -22; //makes yImage go up by 20;
 
-    public static void main(String[] args) {
-        launch(args);
+        }
+        public void update (GraphicsContext gc) {
+            pastX = xImage; //clears old place where the obj was
+            pastY = yImage;
+            gc.clearRect(pastX,pastY, width1, height1);
+
+            xImage+=xVel;
+            if(xImage > width/2){
+                xImage = -xImage;
+            }
+
+
+
+            yVel+= gravity; //if jumping, gravity decreases yVel from -20 to 0 overtime
+
+            yImage += yVel; //gravity, pulling down, disabled when jumping
+
+            if (yImage >= 200) { //if gravity pulls it below the line, then resets yVel to 0;
+                yImage = 200;
+                yVel = 0;
+                canJump = true;
+            }
+
+            //obj always moving right
+            gc.drawImage(playerImage, xImage, yImage, width1, height1);
+        }
     }
 
     //classes
-    public class Line {
-        int x1;
-        int y1;
-        int x2;
-        int y2;
 
-        public Line(int x1, int y1, int x2, int y2) {
-            this.x1 = x1;
-            this.x2 = x2;
-            this.y1 = y1;
-            this.y2 = y2;
-        }
-    }
-
-    public class StickFigure {
-        ArrayList<Line> stickFigure = new ArrayList<Line>();
-        int yVel = 0;
-        int xVel = 0;
-
-
-        public StickFigure() {
-            stickFigure.add(new Line(200, 200, 200, 300));
-            stickFigure.add(new Line(200, 300, 150, 400));
-            stickFigure.add(new Line(200, 300, 250, 400));
-            stickFigure.add(new Line(200, 250, 275, 175));
-            stickFigure.add(new Line(200, 250, 125, 175));
-
-        }
-
-        public void draw(GraphicsContext gc) {
-            for (int i = 0; i < stickFigure.size(); i++) {
-                Line temp = stickFigure.get(i);
-                gc.strokeLine(temp.x1, temp.y1, temp.x2, temp.y2);
-            }
-        }
-
-        public void update(GraphicsContext gc) {
-            if (Dir == move.Right) {
-                xVel = 1;
-            }
-            else if (Dir == move.Left) {
-                xVel = -1;
-            }
-            else {
-                xVel = 0;
-            }
-
-            // Legs end at y2 on indices 1 and 2
-            int lowestY = stickFigure.get(1).y2;
-
-            if (lowestY >= 800) {
-                canJump = true;
-                yVel = 0;
-
-                // Clamp: push figure back up if it sank past floor
-                int overlap = lowestY - 800;
-                for (Line l : stickFigure) {
-                    l.y1 -= overlap;
-                    l.y2 -= overlap;
-                }
-            }
-
-            // Jump: negative yVel moves UP (y decreases)
-            if (Dir == move.Jump && canJump) {
-                yVel = -20;
-                canJump = false;
-            }
-
-            // Gravity pulls DOWN (y increases)
-            yVel += 1;
-
-            for (Line l : stickFigure) {
-                l.x1 += xVel;
-                l.x2 += xVel;
-                l.y1 += yVel;
-                l.y2 += yVel;
-            }
-
-            gc.clearRect(0, 0, width, height);
-            draw(gc);
-        }
-
-    }
 }
